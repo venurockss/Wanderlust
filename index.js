8008d561fcf0c8b320fab22ecfe8cf63d81345d5
venurockss <gajulavenu49@gmail.com>
2024-10-31 09:03:53 +0530

    file name changed

--- index.js
+++ index.js
@@ -97,6 +97,9 @@ app.use(passport.session());
 
 
 app.use("/listing",listing);
+app.get("/", (req, res) => {
+    res.redirect("/listing"); // Redirect the root route to "/listing"
+});
 app.use("/listing/:id/review",reviews);
 app.use("/",userRoute);
 
